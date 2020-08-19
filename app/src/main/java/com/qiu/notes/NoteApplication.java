b2667f345bdc26a4cd4bf9941912e125ba1e21c0
QiuZhiyuan <845026103@qq.com>
2020-08-19 17:46:47 +0800

    create app.java provide base elements

--- app/src/main/java/com/qiu/notes/NoteApplication.java
+++ app/src/main/java/com/qiu/notes/NoteApplication.java
@@ -3,11 +3,13 @@ package com.qiu.notes;
 import android.app.Application;
 
 import com.qiu.notes.data.NoteDatabaseImpl;
+import com.qiu.notes.utils.App;
 
 public class NoteApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        App.i().init(this);
         NoteDatabaseImpl.i().init(this);
     }
 }
